8953cd9ce1e17ef47debade2b9190318d03b1eba
McCoy Didericksen <mccoy.did@gmail.com>
2023-01-29 12:06:18 -0700

    add basic api routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const notes = require('./db/db.json');
 
 const PORT = 3001;
 const app = express();
@@ -10,3 +11,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for retrieving all the notes
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    res.json(notes);
+});
+
+// POST Route for a new note
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    const { title, text } = req.body;
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: notes.length + 1,
+        };
+        notes.push(newNote);
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+        res.json(`Note added successfully 🚀`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
