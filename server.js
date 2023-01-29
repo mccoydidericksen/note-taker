4dadae63b9c9fac95826ae7d5e87ca444f75f319
McCoy Didericksen <mccoy.did@gmail.com>
2023-01-29 12:00:02 -0700

    require needed libs

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
